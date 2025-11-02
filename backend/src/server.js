e4c42f92476b3b13f6bdb32c8835433b1d89a335
Aniket8318 <aniketmaurya8318@gmail.com>
2025-11-02 11:04:01 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 //const express=require('express');
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 import authRoutes from "./routes/auth.route.js"; 
 import messageRoutes from "./routes/message.route.js"; 
@@ -10,12 +13,31 @@ dotenv.config();
 const app=express();
 
 
-const PORT= process.env.PORT || 3000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//const _dirname= path.resolve();
+
+
+
+const PORT = process.env.PORT || 3000;
 
 
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+
+//make ready for deployment 
+if (process.env.NODE_ENV  === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
+
+}
+
 
 
 
